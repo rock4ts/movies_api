6ae1f960f2d8637dbdd8a45302cbaec0ceb2b2bb
mletunenko <mletunenko@gmail.com>
2024-11-13 18:23:13 +0300

    Самое начало, нерабочий код, промежуточный коммит

--- src/api/v1/persons.py
+++ src/api/v1/persons.py
@@ -1,20 +1,20 @@
+from http import HTTPStatus
+
 from typing import List
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
 from pydantic import UUID4
+
 from models.film import Film
 from models.person import Person
 from models.responses import (
     PersonSearchResponse, PersonDetailResponse, PersonFilmListResponse
 )
+from services.person import get_person_service, PersonService
 
-# Объект router, в котором регистрируем обработчики
 router = APIRouter()
 
 
-# Mock для эндпоинта /persons/{uuid}/film
-@router.get(
-    "/{uuid}/film", response_model=List[Film], summary="Фильмы по персоне"
-)
+@router.get("/{uuid}/film", response_model=List[Film], summary="Фильмы по персоне")
 async def persons_films() -> List[Film]:
     """
     Все фильмы по персоне
@@ -22,21 +22,18 @@ async def persons_films() -> List[Film]:
     return PersonFilmListResponse.response      # TODO Заменить Mock на реальный сервис и ответ
 
 
-# Mock для эндпоинта /persons/{uuid}
-@router.get(
-    "/{uuid}", response_model=Person, summary="Данные по персоне"
-)
-async def person_details(uuid: UUID4) -> Person:
+@router.get("/{uuid}", response_model=Person, summary="Данные по персоне")
+async def person_details(uuid: UUID4, person_service: PersonService = Depends(get_person_service)) -> Person:
     """
     Детали по персоне
     """
-    return PersonDetailResponse.response        # TODO Заменить Mock на реальный сервис и ответ
+    person = await person_service.get_by_id(uuid)
+    if not person:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='person not found')
+    return Person(id=person.id, title=person.full_name, films=person.films)
 
 
-# Mock для эндпоинта /persons/search
-@router.get(
-    "/search", response_model=List[Person], summary="Поиск по персонам"
-)
+@router.get("/search", response_model=List[Person], summary="Поиск по персонам")
 async def person_search(query: str) -> List[Person]:
     """
     Эндпоинт для поиска актеров, режиссеров и сценаристов.
---
+++ src/services/person.py
@@ -0,0 +1,98 @@
+from functools import lru_cache
+from typing import Optional
+
+from elasticsearch import AsyncElasticsearch, NotFoundError
+from fastapi import Depends
+from redis.asyncio import Redis
+
+from db.elastic import get_elastic
+from db.redis import get_redis
+from models.person import Person, PersonFilmList
+
+PersonFilmList
+
+PERSON_CACHE_EXPIRE_IN_SECONDS = 60 * 5
+
+class PersonService:
+    def __init__(self,  redis: Redis, elastic: AsyncElasticsearch):
+        self.redis = redis
+        self.elastic = elastic
+
+
+    async def get_by_id(self, person_id: str) -> Optional[Person]:
+        # person = await self._person_from_cache(person_id) # TODO исправить эту и след строчки после реализации кэша
+        person = None
+        if not person:
+            person = await self._get_person_from_elastic(person_id)
+            if not person:
+                return None
+            await self._put_person_to_cache(person)
+        return person
+
+
+    async def _get_person_from_elastic(self, person_uuid: str) -> Optional[Person]:
+        try:
+            person = await self.elastic.get(index='persons', id=person_uuid)
+            films_query = {
+                "query": {
+                    "bool": {
+                        "should": [
+                            {
+                                "nested": {
+                                    "path": "directors",
+                                    "query": {
+                                        "term": {
+                                            "directors.id": person_uuid
+                                        }
+                                    }
+                                }
+                            }
+                            # {"terms": {"actors.uuid": [str(person_uuid)]}},
+                            # {"terms": {"writers.uuid": [str(person_uuid)]}},
+                            # {"terms": {"directors.uuid": [str(person_uuid)]}}
+                        ]
+                    }
+                }
+            }
+            films_response = await self.elastic.search(index="movies", body=films_query)
+            films = []
+            for film in films_response['hits']['hits']:
+                film_data = film['_source']
+                roles = []
+
+                if any(actor['id'] == str(person_uuid) for actor in film_data.get('actors', [])):
+                    roles.append("actor")
+                if any(writer['id'] == str(person_uuid) for writer in film_data.get('writers', [])):
+                    roles.append("writer")
+                if any(director['id'] == str(person_uuid) for director in film_data.get('directors', [])):
+                    roles.append("director")
+
+                if roles:
+                    films.append(PersonFilmList(
+                        uuid=film_data['id'],
+                        roles=roles
+                    ))
+            person['_source']['films'] = films
+            print(f'-------Person: {person}')
+        except NotFoundError:
+            return None
+        return Person(**person['_source'])
+
+
+    async def _person_from_cache(self, person_id: str) -> Optional[Person]:
+        data = await self.redis.get(person_id)
+        if not data:
+            return None
+        person = Person.parse_raw(data)
+        return person
+
+    async def _put_person_to_cache(self, person: Person):
+        await self.redis.set(person.id, person.json(), PERSON_CACHE_EXPIRE_IN_SECONDS)
+
+
+@lru_cache()
+def get_person_service(
+        redis: Redis = Depends(get_redis),
+        elastic: AsyncElasticsearch = Depends(get_elastic),
+) -> PersonService:
+    return PersonService(redis, elastic)
