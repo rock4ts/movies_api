deba39a5304a6b41b854223f46184525a8563173
rock4ts <rock4ts@gmail.com>
2024-11-16 12:44:35 +0300

    remove excessive dotenv loading

--- src/core/config.py
+++ src/core/config.py
@@ -3,10 +3,6 @@ from logging import config as logging_config
 
 from core.logger import LOGGING
 
-from dotenv import load_dotenv
-
-load_dotenv()
-
 # Применяем настройки логирования
 logging_config.dictConfig(LOGGING)
 
