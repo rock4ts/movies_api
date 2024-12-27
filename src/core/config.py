2a1b2d89178c090a29f3797db9b18f1654dbcbf8
rock4ts <rock4ts@gmail.com>
2024-12-27 18:43:00 +0300

    Конфига форматера ruff, форматирование core.cofig

--- src/core/config.py
+++ src/core/config.py
@@ -10,16 +10,19 @@ logging_config.dictConfig(LOGGING)
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict()
-    project_name: str = 'Some project name'
-    redis_host: str = '127.0.0.1'
+    project_name: str = "Some project name"
+    redis_host: str = "127.0.0.1"
     redis_port: int = 6379
-    elastic_host: str = '127.0.0.1'
+    elastic_host: str = "127.0.0.1"
     elastic_port: int = 9200
     debug: bool = False
 
+    authjwt_secret_key: str
+    authjwt_algorithm: str = "RS256"
+
     @property
-    def elastic_url(self):
-        return f'http://{self.elastic_host}:{self.elastic_port}'
+    def elastic_url(self) -> str:
+        return f"http://{self.elastic_host}:{self.elastic_port}"
 
 
 settings = Settings()
